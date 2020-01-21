3bec5d5079cd30b8eac75922cad6dccdf925de39
coderbase-it <coderbaseit@gmail.com>
2020-01-21 17:26:47 +0100

    1 file modified, 6 added

---
+++ src/app/pony-image/pony-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pony-image',
+  templateUrl: './pony-image.component.html',
+  styleUrls: ['./pony-image.component.css']
+})
+export class PonyImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
